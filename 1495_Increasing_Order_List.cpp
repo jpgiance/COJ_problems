a43ed596f4930eabe9285acc8f6621dd7e6634d7
jpgiance <jpgiance@gmail.com>
2017-07-18 23:49:52 -0500

    First of sorting problems

---
+++ 1495_Increasing_Order_List.cpp
@@ -0,0 +1,37 @@
+/* 
+1495 - Increasing Order List
+
+Statistics	Sub: 4993 | AC: 3314 | AC%: 66,37 | Score: 0,07
+Created by	Yonny Mondelo Hernández
+Added by	ejaltuna (2011-10-13)
+Limits	
+Total Time: 10000 MS | Test Time: 2000 MS |Memory: 62 MB | Output: 64 MB | Size: 30 KB
+Enabled languages	
+Available in	  
+Description
+
+You are given a list of 1000 integers at most, and you must order its elements ascending.
+Input specification
+
+The first line of the input is N, the number of elements in the list. In each of the N following lines, there are one positive integer number 1 <= Ni <= 1000.
+Output specification
+
+You must print the same (N) numbers, one per line, but now in ascending order.
+Sample input
+
+3
+15
+35
+1
+Sample output
+
+1
+15
+35
+
+*/
+
+
+/* Code */
+
+
