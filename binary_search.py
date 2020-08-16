466c50505042ae870aaccc1a336ef43942f1b1d2
jorgepg <jpgiance@gmail.com>
2020-08-16 17:27:55 -0500

    first commit binary search question

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+""" 
+Write a function that takes in a sorted array of integers as well as a target integer.
+This function should use the binary search algorith to determine if the target integer is
+contained in the array and should returns its index if it is, otherwise -1
+
+Sample input
+arrar = [0, 1, 21, 33, 45, 45, 61, 71] 
+target = 33
+
+Sample output
+3
+"""
